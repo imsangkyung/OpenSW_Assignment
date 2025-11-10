5ec145718d8ff68c9473e7bf9f633550d5d6bbe7
Sangkyung Im <imsangkyung@gmail.com>
2025-11-10 14:38:24 +0900

    add my baisc ops

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "myops.h"
 
 double calculate(char* op, double a, double b){
     printf("Calculating.....\n");
+
+    if(strcmp(op, "+") == 0){
+        return myadd(a, b);
+    }
+    else if (strcmp(op, "-") == 0){
+        return mysub(a, b);
+    }
+    else if (strcmp(op, "*") == 0){
+        return mymul(a, b);
+    }
+    else if(strcmp(op, "/") == 0){
+        return mydiv(a, b);
+    }
+    else {
+        printf("Unsupported operation: %s\n", op);
+        return 0;
+    }
 }
 
 int main(void){
@@ -10,7 +30,7 @@ int main(void){
 
     printf("~~~ Welcome to my calculator  ~~~\n");
     printf("Enter expression (e.g. 1 + 2): ");
-    scanf("%lf $s %lf", &a, op, &b);
+    scanf("%lf %s %lf", &a, op, &b);
     printf("Result = %.2lf\n", calculate(op, a, b));
     
     return 0;
